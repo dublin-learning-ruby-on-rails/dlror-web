e5a9e37c85426620b830a1b69cd72ac80fd658d2
Jules Roman B. Polidario <jrpolidario@gmail.com>
2016-05-31 22:38:44 +0100

    DRYed up event partial view file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def time_in_relative_words(time)
+    time > DateTime.now ? distance_of_time_in_words_to_now(time) + ' from now' : time_ago_in_words(time) + ' ago'
+  end
 end
