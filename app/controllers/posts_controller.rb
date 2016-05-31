e481252c256feaca5b45bd9a47e33eee0edb2cb5
Jules Roman B. Polidario <jrpolidario@gmail.com>
2016-05-31 23:05:25 +0100

    set up devise authentication; updated layouts; update cancancan authorisation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
+
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
