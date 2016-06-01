8619b1afc6423c6615ee77325e610646b5ac35cd
Jules Roman B. Polidario <jrpolidario@gmail.com>
2016-06-01 01:01:09 +0100

    added logout link and fixed redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Devise
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || events_path
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
   end
